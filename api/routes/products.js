b27aca52053a2da0fa2feecca098c5a211f50ca6
hectortav <tavhector@gmail.com>
2019-07-24 18:28:07 +0300

    error handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,35 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-	res.status(200).json({
+	res.status(201).json({
 		message: 'POST requests under /products'
+	});
+});
+
+router.get('/:productId', (req, res, next) => {
+	const id = req.params.productId;
+	if (id === 'test') {
+		res.status(200).json({
+			message: 'This is a test case'
+		})
+	}
+	else {
+		res.status(200).json({
+			message: 'You passed an ID',
+			id: id
+		});
+	}
+});
+
+router.patch('/:productId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Updated product'
+	});
+});
+
+router.delete('/:productId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Deleted product'
 	});
 });
 
