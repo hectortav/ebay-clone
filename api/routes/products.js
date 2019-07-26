6b063b0533d24afc57a00639710e7eb9093cf806
hectortav <tavhector@gmail.com>
2019-07-27 01:17:56 +0300

    add, see and delete products

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,18 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'GET requests under /products'
-	});
+	Product.find()
+	.exec()
+	.then(docs => {
+		console.log(docs);
+		res.status(200).json(docs);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
+	})
 });
 
 router.post('/', (req, res, next) => {
@@ -16,28 +25,40 @@ router.post('/', (req, res, next) => {
 		name: req.body.name,
 		price: req.body.price
 	});
-	product.save().then(result => {
+	product
+	.save()
+	.then(result => {
 		console.log(result);
-	});
-	res.status(201).json({
-		message: 'POST requests under /products',
-		createdProduct: product
+		res.status(201).json({
+			message: 'POST requests under /products',
+			createdProduct: result
+		});
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
 	});
 });
 
 router.get('/:productId', (req, res, next) => {
 	const id = req.params.productId;
-	if (id === 'test') {
-		res.status(200).json({
-			message: 'This is a test case'
-		})
-	}
-	else {
-		res.status(200).json({
-			message: 'You passed an ID',
-			id: id
-		});
-	}
+	Product.findById(id)
+	.exec()
+	.then(doc => {
+		console.log("From database", doc);
+		if (doc)
+		{
+			res.status(200).json(doc);
+		}
+		else
+		{
+			res.status(404).json({message: 'No entry'});
+		}
+	}).catch(err => {
+	console.log(err);
+	res.status(500).json({error: err});
+	});
 });
 
 router.patch('/:productId', (req, res, next) => {
@@ -47,9 +68,18 @@ router.patch('/:productId', (req, res, next) => {
 });
 
 router.delete('/:productId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Deleted product'
-	});
+	const id = req.params.productId;
+	Product.remove({_id: id})
+	.exec()
+	.then(result => {
+		res.status(200).json(result);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
+	})
 });
 
 module.exports = router;
