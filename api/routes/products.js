53830b2456839e02d99148575761ac5e9b566c90
hectortav <tavhector@gmail.com>
2019-07-27 01:30:24 +0300

    patch works too

--- api/routes/products.js
+++ api/routes/products.js
@@ -62,8 +62,24 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Updated product'
+	const id = req.params.productId;
+	const updateOps = {};
+	for (const ops of req.body) 
+	{
+		updateOps[ops.propName] = ops.value;
+	}
+
+	Product.update({_id: id}, { $set: updateOps })
+	.exec()
+	.then(result => {
+		console.log(result);
+		res.status(200).json(result);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
 	});
 });
 
