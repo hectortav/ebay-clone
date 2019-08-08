84c8e87dff44c70bc81ceab912205e2c61ee1055
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-08-08 18:27:29 +0300

    signup code

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
+import { Router } from '@angular/router';
+import { EnrollmentService } from '../enrollment.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +11,17 @@ import { registerLocaleData } from '@angular/common';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private enrollmentService: EnrollmentService, private router: Router) { }
 
   ngOnInit() {
   }
 
   register(form) {
     console.log(form.value);
+    this.enrollmentService.register(form.value).subscribe((res) => {
+      console.log("Signed Up!");
+      this.router.navigateByUrl('/home');
+    });
   }
 
 }
