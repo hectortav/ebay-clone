4e35709d6989cf54cd8c71a5d02e808ba5e2bf94
hectortav <tavhector@gmail.com>
2019-08-13 18:21:53 +0300

    auction database and rest functions

--- api/models/auction.js
+++ api/models/auction.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 
 const auctionSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	product: { type: mongoose.Schema.Types.ObjectId, red: 'Product' , required: true},
+	//product: { type: mongoose.Schema.Types.ObjectId, red: 'Product' , required: true},
+	name: { type: String, required: true },
+	category: { type: String, required: true },
+	location: { type: String, required: true },
+	country: { type: String, required: true },
 	currently: { type: Number, required: true },
 	first_bid: { type: Number},
 	no_bids: { type: Number, default: 0 },
 	started: {type: Date, required: true },
 	ends: {type: Date, required: true },
 	description: { type: String },
-	//seller: { type: mongoose.Schema.Types.ObjectId, red: 'Bidder' , required: true},
-
-	buy_price: { type: Number},
+	seller: { type: mongoose.Schema.Types.ObjectId, red: 'User' , required: true},
+	buy_price: { type: Number}
 	/*bids: {
 		type: Map,
 		of: mongoose.Schema.Types.ObjectId,
--- api/routes/auctions.js
+++ api/routes/auctions.js
@@ -5,18 +5,39 @@ const mongoose = require('mongoose');
 const Auction = require('../models/auction');
 const Product = require('../models/product');
 
+/*
+	name: { type: String, required: true },
+	category: { type: String, required: true },
+	location: { type: String, required: true },
+	country: { type: String, required: true },
+	currently: { type: Number, required: true },
+	first_bid: { type: Number},
+	no_bids: { type: Number, default: 0 },
+	started: {type: Date, required: true },
+	ends: {type: Date, required: true },
+	description: { type: String },
+	seller: { type: mongoose.Schema.Types.ObjectId, red: 'User' , required: true},
+	buy_price: { type: Number}
+*/
 
 router.get('/', (req, res, next) => {
-	Auction.find().select('product _id currently first_bid no_bids').exec().then(docs => {
+	Auction.find().select('_id name category location country currently first_bid no_bids started ends decription seller').exec().then(docs => {
 		res.status(200).json({
 			count: docs.length,
 			auctions: docs.map(doc => {
 				return {
 					_id: doc._id,
-					product: doc.product,
+					name: doc.name,
+					category: doc.category,
+					location: doc.location,
+					country: doc.country,
 					currently: doc.currently,
 					first_bid: doc.first_bid,
 					no_bids: doc.no_bids,
+					started: doc.started,
+					ends: doc.ends,
+					decription: doc.description,
+					seller: doc.seller,
 					request: {
 						type: 'GET',
 						url: 'http://localhost:3000/auctions/' + doc._id
