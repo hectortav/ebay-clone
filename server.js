6fc7866488c56dbd1a73318b543ae26e4a69cc6e
hectortav <tavhector@gmail.com>
2019-08-20 20:27:28 +0300

    get user's auctions

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const https = require('https');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
@@ -7,3 +6,18 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
+
+/*
+var tls = require('tls');
+var fs = require('fs');
+
+var options = {
+ key : fs.readFileSync('mykey.pem'),
+ cert: fs.readFileSync('my-cert.pem')
+}
+
+tls.createServer(options,function(s)
+{
+ s.write("welcome!\n");
+ s.pipe(s);
+}).listen(3000);*/
