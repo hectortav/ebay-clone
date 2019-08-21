9f598d5aaec97c6a49e841774c05d5766db04771
hectortav <tavhector@gmail.com>
2019-08-21 16:23:59 +0300

    update auctions with put

--- api/routes/auctions.js
+++ api/routes/auctions.js
@@ -184,4 +184,30 @@ router.delete('/:auctionId', (req, res, next) => {
 	});
 });
 
+router.put('/:auctionId', (req, res, next) => {
+	const id = req.params.auctionId;
+	const updateOps = {};
+	for (const ops of req.body) {
+		updateOps[ops.property] = ops.value;
+		console.log(updateOps);
+	}
+	Auction.update({ _id: id }, { $set: updateOps })
+	.exec()
+	.then(result => {
+		res.status(200).json({
+			message: 'Auction updated',
+			request: {
+				type: 'GET',
+				url: 'http://localhost:3000/auctions/' + id
+			}
+		});
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
+	});
+});
+
 module.exports = router;
