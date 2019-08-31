32c5f62a92d56d60ca6d086d547f44894e35b43d
hectortav <tavhector@gmail.com>
2019-08-31 16:57:49 +0300

    get received messages

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -9,18 +9,52 @@ const Message = require('../models/message');
 
 router.get('/:userId/sent', (req, res, next) => {
 	User.find({ sender: userId})
-	.select('_id sender receiver text')
+	.select('_id sender receiver time text')
 	.exec()
 	.then(docs => {
-		return res.status(200).json({
-            _id: doc._id,
-            sender: doc.sender,
-            receiver: doc.receiver,
-            subject: doc.subject,
-            time: doc.time,
-            text: doc.text
-        });
+        const response = {
+            count: docs.length,
+            users: docs.map(doc => {
+                    return {
+                            _id: doc._id,
+                            sender: doc.sender,
+                            receiver: doc.receiver,
+                            subject: doc.subject,
+                            time: doc.time,
+                            text: doc.text
+                    }
+                })
+            };
+            res.status(200).json(response);
+        })
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
 	})
+});
+
+router.get('/:userId/received', (req, res, next) => {
+	User.find({ received: userId})
+	.select('_id sender receiver time text')
+	.exec()
+	.then(docs => {
+        const response = {
+            count: docs.length,
+            users: docs.map(doc => {
+                    return {
+                            _id: doc._id,
+                            sender: doc.sender,
+                            receiver: doc.receiver,
+                            subject: doc.subject,
+                            time: doc.time,
+                            text: doc.text
+                    }
+                })
+            };
+            res.status(200).json(response);
+        })
 	.catch(err => {
 		console.log(err);
 		res.status(500).json({
@@ -31,58 +65,14 @@ router.get('/:userId/sent', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
 	
-	});
-
-router.post('/login', (req, res, next) => {
-	User.find({ email: req.body.email})
-	.exec()
-	.then(user => {
-		if (user.length < 1) {
-			return res.status(401).json({
-				message: 'Auth Failed'
-			});
-		}
-		bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-			if (err) {
-				return res.status(401).json({
-					message: 'Auth Failed'
-				});
-			}
-				if (result) {
-					const token = jwt.sign(
-					{
-						email: user[0].email,
-						userId: user[0]._id,
-						role: user[0].role
-					},
-					process.env.JWT_KEY,
-					{
-						expiresIn: "1h"
-					});
-					return res.status(200).json({
-						message: 'Auth Successful',
-						token: token
-					});
-				}
-				res.status(401).json({
-					message: 'Auth Failed'
-				});
-		});
-	})
-	.catch(err => {
-		console.log(err);
-		res.status(500).json({
-			error: err
-		});
-	});
 });
 
-router.delete('/:userId', (req, res, next) => {
-	User.remove({ _id: req.params.userId})
+router.delete('/:messageId', (req, res, next) => {
+	Message.remove({ _id: req.params.messageId})
 	.exec()
 	.then(result => {
 		res.status(200).json({
-			message: 'User Deleted'
+			message: 'Message Deleted'
 		})
 	})
 	.catch(err => {
