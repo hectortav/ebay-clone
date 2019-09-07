95c5bda6dbe83d9f87a5bebcc26e377001cebb51
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-09-07 21:06:45 +0300

    messaging system workign

--- front-end/src/app/inbox/inbox.component.ts
+++ front-end/src/app/inbox/inbox.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { Message } from '../_models';
+import { MessagesService } from '../_services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlertService } from '../_alert';
 
 @Component({
   selector: 'app-inbox',
@@ -6,10 +11,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbox.component.css']
 })
 export class InboxComponent implements OnInit {
+  messages: Message[];
+  selectedMessage: Message;
+  loading: boolean = false;
+  replyBox: boolean = false;
+  messageForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private messagesService: MessagesService,
+    private formBuilder: FormBuilder,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.loadAllInbox();
+
+    this.messageForm = this.formBuilder.group({
+      text: ['', Validators.required],
+      subject: ['', Validators.required],
+      sender: [''],
+      receiver: ['']
+    });
   }
 
+  private loadAllInbox() {
+    this.loading = true;
+    this.messagesService.getReceived().pipe(first()).subscribe(res => {
+      this.loading = false;
+      let newObj: any = res;
+      this.messages = newObj.messages;
+    });
+  }
+
+  onSelect(message: Message): void {
+    this.selectedMessage = message;
+    message.read = true;
+    this.messagesService.updateMessage(message).subscribe();
+  }
+
+  delete(message: Message): void {
+    this.messagesService.deleteMessage(message).subscribe();
+    window.location.reload();
+  }
+
+  reply() {
+    this.replyBox = !this.replyBox;
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.messageForm.controls; }
+
+  sendMessage() {
+    console.log(this.messageForm);
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.messageForm.value.sender = this.selectedMessage.receiver;
+    this.messageForm.value.receiver = this.selectedMessage.sender;
+
+    this.loading = true;
+    this.messagesService.newMessage(this.messageForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          window.location.reload();
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
 }
--- front-end/src/app/sent/sent.component.ts
+++ front-end/src/app/sent/sent.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { Message } from '../_models';
+import { MessagesService } from '../_services';
 
 @Component({
   selector: 'app-sent',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sent.component.css']
 })
 export class SentComponent implements OnInit {
+  messages: Message[];
+  selectedMessage: Message;
+  loading: boolean = false;
 
-  constructor() { }
+  constructor(private messagesService: MessagesService) { }
 
   ngOnInit() {
+    this.loadAllSent();
+  }
+
+  private loadAllSent() {
+    this.loading = true;
+    this.messagesService.getSent().pipe(first()).subscribe(res => {
+      this.loading = false;
+      let newObj: any = res;
+      this.messages = newObj.messages;
+    });
+  }
+
+  onSelect(message: Message): void {
+    this.selectedMessage = message;
+    this.messagesService.updateMessage(message).subscribe();
+    console.log(message);
   }
 
 }
